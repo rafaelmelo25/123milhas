59986c4ef8ceecd7eceaa47711610ee6ddce5bd9
rafaelmelo25 <rafael.melo.p@hotmail.com>
2019-07-25 23:35:50 -0300

    Add files via upload

---
+++ questao2.php
@@ -0,0 +1,22 @@
+<?php
+
+$manobrista = 1;
+$veiculo = 1;
+$vagas = 30;
+
+$cont = 1; 
+
+   
+	if ($vagas <= 30) {
+   echo "O". $manobrista . "pode guardar o ". $veiculo; 	                  
+        
+    } else {       
+   echo "O". $manobrista . "não pode guardar o ". $veiculo;
+
+
+<?php $cont++; >
+}
+
+
+
+?>
